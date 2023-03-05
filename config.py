3af08fb49bfc310b2dbad9374e282e17601ef5b6
ItzNek1T <ItzNek1T@mail.com>
2023-03-05 17:10:44 +0600

    Add config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+# API endpoint for the conversational AI service
+URL = "https://chatgpt-api.shn.hk/v1/"
+
+# Name of the conversational AI model to use
+MODEL = "gpt-3.5-turbo"
+
+# Telegram bot API token
+TOKEN = "YOUR_API_TOKEN"
